8faac0aef86aeefcb323324ca6d6414e833c4388
cosmin <cosmin@DESKTOP-74IFNGK>
2023-03-16 09:44:52 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferma
+{
+    public class Animal
+    {
+        public string nume { get; set; }
+        public string culoare { get; set; }
+        public double greutate { get; set; }
+        public double varsta { get; set; }
+        public string tip { get; set; }
+        public Animal()
+        {
+            nume = "";
+            greutate = 0;
+            varsta = 0;
+            culoare = "";
+            tip = "";
+        }
+        public Animal(string nume="nedefinit",double greutate=0,double varsta = 0,string culoare="nedfinit",string tip="M")
+        {
+            this.nume = nume;
+            this.greutate = greutate;
+            this.varsta = varsta;
+            this.culoare = culoare;
+            this.tip = tip;
+        }
+        public Animal(Animal A)
+        {
+            nume = A.nume;
+            greutate = A.greutate;
+            varsta = A.varsta;
+            culoare = A.culoare;
+            tip = A.tip;
+        }
+        //public string Nume { get; set; }
+        //public float Varsta { get; set; }
+        //public float Greutate { get; set; }
+        /// <summary>
+        ///public string Culoare { get; set; }
+        /// </summary>
+        //public string Tip { get; set; }
+    }
+    class Cow:Animal
+    {
+        double litriiLapte { get; set; }
+        bool gestanta { get; set; }
+        int nrVitei { get; set; }
+        public Cow():base()
+        {
+            litriiLapte = 0;
+            gestanta = false;
+            nrVitei = 0;
+        }
+        public Cow(string nume = "nedefinit", double greutate = 0, double varsta = 0, string culoare = "nedfinit", string tip = "M", double litriiLapte = 0,bool gestanta=false,int nrVitei=0):base(nume,greutate,varsta,culoare,tip)
+        {
+            this.litriiLapte = litriiLapte;
+            this.gestanta = gestanta;
+            this.nrVitei = nrVitei;
+        }
+        public Cow(Cow C):base(C.nume,C.greutate,C.varsta,C.culoare,C.tip)
+        {
+            litriiLapte = C.litriiLapte;
+            gestanta = C.gestanta;
+            nrVitei = C.nrVitei;
+        }
+    }
+    class Horse:Animal
+    {
+
+    }
+    class Hen:Animal
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Ferma;
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal A = new Animal("geta",2.3,0.5,"rosu");
+        Console.WriteLine(A.nume + ' ' + A.culoare);
+
+    }
+}
